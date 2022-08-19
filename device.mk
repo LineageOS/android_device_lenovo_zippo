e3eafae1005cf97b136bc8fcb8ccd9f59aa4b07f
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:48:18 +0200

    zippo: copy prebuilt libhidlbase from vndk v32
    Change-Id: Icee7173c7d84deb77290a8ab47b747995b2bedfb
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
 
