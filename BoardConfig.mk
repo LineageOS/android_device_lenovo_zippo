6bb1a91e18049bee0715854362ab8e5c7c807dc5
Einar Gednochsson <pete80pro.pb@gmail.com>
2024-03-29 12:11:23 +0100

    zippo: define vendor security patch level
    Change-Id: I37a64c188114650f75c3ac29e84f9d4215096af7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,5 +34,8 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/gesture_on"
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Vendor Security patch level
+VENDOR_SECURITY_PATCH := 2021-08-01
+
 # Inherit from the proprietary version
 -include vendor/lenovo/zippo/BoardConfigVendor.mk
