2ad3f60c0acf6611b5f561857a0cc3a743a12eff
Michael Bestas <mkbestas@lineageos.org>
2025-01-02 01:12:07 +0200

    zippo: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2024 The LineageOS Project
+# Copyright (C) 2019-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,6 +7,7 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Lineage stuff.
+TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
