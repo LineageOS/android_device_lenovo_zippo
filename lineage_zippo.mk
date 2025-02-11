6a824f4e8bce1e954aaa239f25fcafe8ff85f847
Marc Bourgoin <themard85@gmail.com>
2025-02-10 20:22:05 -0700

    zippo: Decommonize product inherits
    Change-Id: I92adf967e59401b941b95e5706c8c28020fc40c4

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from zippo device
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common Lineage stuff.
