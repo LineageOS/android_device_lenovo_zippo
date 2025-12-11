3550e47a2aff5e7e63f888c09c59bc9e78fef006
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    zippo: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -50,6 +50,14 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('remote_handle64_invoke')
         .clear_symbol_version('remote_handle64_open')
         .clear_symbol_version('remote_register_dma_handle'),
+    (
+        'vendor/lib64/hw/android.hardware.camera.provider@2.4-impl.so',
+        'vendor/lib64/android.hardware.camera.provider@2.4-external.so',
+        'vendor/lib64/camera.device@3.4-external-impl.so',
+        'vendor/lib64/camera.device@3.5-external-impl.so',
+        'vendor/lib64/camera.device@3.6-external-impl.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
